cce160692c87b1115909a3b55504947dc48c2121
Don Piero <77784863+DonPiero@users.noreply.github.com>
2026-09-05 00:53:17 +0300

    Add: pow-x-n - submission-0

---
+++ Data Structures & Algorithms/pow-x-n/submission-0.py
@@ -0,0 +1,16 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        def helper(x, n):
+            if x == 0:
+                return 0
+            if n == 0:
+                return 1
+
+            res = helper(x, n // 2) 
+            if n % 2 == 1:
+                return x * res * res 
+            else:
+                return res * res
+        
+        res = helper(x, abs(n))
+        return res if n >= 0 else 1 / res
